6cc2f60d232c843e972979652a486f9f76c96e85
punits009 <52010207+punits009@users.noreply.github.com>
2019-07-12 10:38:36 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, OnInit} from '@angular/core';
+import { Service } from './service';
+
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+
+export class AppComponent implements OnInit{
+    constructor(private service: Service) {
+
+    }
+    public status=""
+    public value=""
+    temp: any[] =[]
+    
+       ngOnInit() {
+        this.service.getData().subscribe((data: any) =>this.temp = data)
+    }
+}
