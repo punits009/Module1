3664031ac3c33b58562d8ee7fbf1335b1ceb39e2
punits009 <52010207+punits009@users.noreply.github.com>
2019-07-15 14:07:58 +0530

    Add files via upload

---
+++ LoginFormDemo/src/com/cg/dao/LoginDaoImpl.java
@@ -0,0 +1,41 @@
+package com.cg.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.cg.bean.Login;
+import com.cg.util.DBUtil;
+
+public class LoginDaoImpl implements LoginDao{
+
+	Connection con=DBUtil.getConnect();
+	@Override
+	public Login loginUser(Login user) {
+		// TODO Auto-generated method stub
+		Login obj=null;
+		String qry="select * from UserDetails";
+		try
+		{
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery(qry);
+			while(rs.next())
+			{
+				String uName=rs.getString(1);
+				String pwd=rs.getString(2);
+				if(uName.equals(user.getUsername()) && pwd.equals(user.getPassword()))
+				{
+					obj=user;
+					break;
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		return obj;
+	}
+	
+
+}
---
+++ LoginFormDemo/src/com/cg/service/LoginService.java
@@ -0,0 +1,8 @@
+package com.cg.service;
+
+import com.cg.bean.Login;
+
+public interface LoginService {
+	public Login loginUser(Login user);
+
+}
